5703129f30114031313b1121ffa14636616893fa
szymonkrygier <skprimedev@gmail.com>
2022-01-09 13:34:07 +0100

    Added server class

---
+++ server/server.py
@@ -0,0 +1,6 @@
+# Komunikator P2P z centralnym serwerem
+# Szymon Krygier WCY19IJ1N1
+
+class Server:
+    def __init__(self):
+        pass
--- server_launch.py
+++ server_launch.py
@@ -1,3 +1,10 @@
 # Komunikator P2P z centralnym serwerem
 # Szymon Krygier WCY19IJ1N1
 import common.check_runtime
+
+def run():
+    server_instance = Server()
+
+
+if __name__ == "__main__":
+    run()
