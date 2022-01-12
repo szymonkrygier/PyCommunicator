4b11271a74931874fd21d7f1bad91bce0975e90b
szymonkrygier <skprimedev@gmail.com>
2022-01-12 23:17:13 +0100

    Code clean-up

--- common/check_runtime.py
+++ common/check_runtime.py
@@ -1,17 +1,19 @@
 # Komunikator P2P z centralnym serwerem
 # Szymon Krygier WCY19IJ1N1
 import sys
+
 import importlib.util
 
 # Sprawdzenie wersji Pythona
 if sys.version_info < (3, 9, 7):
-    print("Twoja wersja Pythona ({0}) nie jest wspierana przez aplikacje. Wymagana jest co najmniej wersja 3.9.7.".format(sys.version))
+    print("Twoja wersja Pythona ({0}) nie jest wspierana przez aplikacje. Wymagana jest co najmniej wersja 3.9.7.".
+          format(sys.version))
     exit(1)
 
 # Sprawdzenie czy wymagane moduly sa dostepne
 requiredModules = ['socket', 'PySide6', 'threading', 'array', 'dataclasses', 'datetime', 'signal', 'typing']
 
 for module in requiredModules:
-    if not module in sys.modules and importlib.util.find_spec(module) == None:
+    if not module in sys.modules and importlib.util.find_spec(module) is None:
         print("Wymagany modul {0} nie zostal znaleziony! Aplikacja zostanie zaknieta.".format(module))
         exit(1)
